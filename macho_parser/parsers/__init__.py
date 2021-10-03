5078b75626115058eb4659763632017609b2f47b
Reuben Sammut <reubensammut@gmail.com>
2021-10-03 23:26:52 +0200

    Added parsing of LC_SEGMENT_64

--- macho_parser/parsers/__init__.py
+++ macho_parser/parsers/__init__.py
@@ -4,7 +4,7 @@ import struct
 import sys
 
 from macho_parser.util import TabbedWriter
-from macho_parser.parsers.constants import get_cpu_type, get_cpu_subtype
+from macho_parser.parsers.constants import get_cpu_type, get_cpu_subtype, get_filetype, get_flags, get_command, get_sflags, get_prots
 
 class Parser():
     def __init__(self, path):
@@ -20,11 +20,79 @@ class Parser():
             0xCFFAEDFE: (self.parse_64bit,  "<")    # Little-Endian 64-bit binary
         }
 
+        self.cmd_parsers = {
+            0x19: self.parse_lc_segment_64
+        }
+
+    def parse_lc_segment_64(self, endian, fh):
+        segname, vmaddr, vmsize, fileoff, filesize, maxprot, initprot, nsects, flags = unpack(f"{endian}16sQQQQIIII", fh.read(4*4 + 16 + 8*4))
+        segname = segname.rstrip(b"\x00").decode()
+        
+        with self.w.next_level() as w1:
+            w1.tprint(f"Segment name:       {segname}")
+            w1.tprint(f"VM Address:         {hex(vmaddr)}")
+            w1.tprint(f"VM Size:            {hex(vmsize)}")
+            w1.tprint(f"File Offset:        {hex(fileoff)}")
+            w1.tprint(f"File Size:          {hex(filesize)}")
+            w1.tprint(f"Maximum Prot:       {get_prots(maxprot)} ({hex(maxprot)})")
+            w1.tprint(f"Initial Prot:       {get_prots(initprot)} ({hex(initprot)})")
+            w1.tprint(f"Number of sects:    {nsects}")
+            w1.tprint(f"Flags:              {hex(flags)}")
+            for j in range(nsects):
+                sectname, ssegname, saddr, ssize, soffset, salign, sreloff, snreloc, sflags, sres1, sres2, sres3 = unpack(f"{endian}16s16sQQIIIIIIII", fh.read(16*2 + 8*2 + 4*8))
+                sectname = sectname.rstrip(b"\x00").decode()
+                ssegname = ssegname.rstrip(b"\x00").decode()
+                w1.tprint(f"SECT[{j}]")
+                with w1.next_level() as w2:
+                    w2.tprint(f"Section name:       {sectname}")
+                    w2.tprint(f"Segment address:    {hex(saddr)}")
+                    w2.tprint(f"Size:               {hex(ssize)}")
+                    w2.tprint(f"Offset:             {hex(soffset)}")
+                    w2.tprint(f"Align:              {hex(salign)}")
+                    w2.tprint(f"Relocations offset: {hex(sreloff)}")
+                    w2.tprint(f"Number relocations: {hex(snreloc)}")
+                    w2.tprint(f"Segment flags:      {get_sflags(sflags)} ({hex(sflags)})")
+                    w2.tprint(f"Reserved1:          {hex(sres1)}")
+                    w2.tprint(f"Reserved2:          {hex(sres2)}")
+                    w2.tprint(f"Reserved3:          {hex(sres3)}")
+
+    def parse_lc_generic(self, endian, fh):
+        pass 
+
     def parse_32bit(self, endian, fh):
         self.w.tprint("32-bit Mach-O")
+        cpu_type, cpu_subtype, filetype, ncmds, sizeofcmds, flags = unpack(f"{endian}IIIIIII", fh.read(4*6))
+        self.w.tprint(f"CPU Type:       {get_cpu_type(cpu_type)} ({hex(cpu_type)})")
+        self.w.tprint(f"CPU SubType:    {get_cpu_subtype(cpu_type, cpu_subtype)} ({hex(cpu_subtype)})")
+        self.w.tprint(f"File Type:      {get_filetype(filetype)} ({hex(filetype)})")
+        self.w.tprint(f"Number cmds:    {ncmds}")
+        self.w.tprint(f"Size of cmds:   {sizeofcmds}")
+        self.w.tprint(f"Flags:          {get_flags(flags)} ({hex(flags)})")
 
     def parse_64bit(self, endian, fh):
         self.w.tprint("64-bit Mach-O")
+        cpu_type, cpu_subtype, filetype, ncmds, sizeofcmds, flags, reserved = unpack(f"{endian}IIIIIII", fh.read(4*7))
+        self.w.tprint(f"CPU Type:       {get_cpu_type(cpu_type)} ({hex(cpu_type)})")
+        self.w.tprint(f"CPU SubType:    {get_cpu_subtype(cpu_type, cpu_subtype)} ({hex(cpu_subtype)})")
+        self.w.tprint(f"File Type:      {get_filetype(filetype)} ({hex(filetype)})")
+        self.w.tprint(f"Number cmds:    {ncmds}")
+        self.w.tprint(f"Size of cmds:   {sizeofcmds}")
+        self.w.tprint(f"Flags:          {get_flags(flags)} ({hex(flags)})")
+        self.w.tprint(f"Reserved:       {hex(reserved)}")
+
+        for i in range(ncmds):
+            cmd, cmdsize = unpack(f"{endian}II", fh.read(4*2))
+            self.w.tprint(f"LOAD COMMAND[{i}] - {get_command(cmd)} ({hex(cmd)}) [{hex(cmdsize)}]")
+
+            new_parser = self.cmd_parsers.get(cmd)
+
+            if new_parser is None:
+                cmdsize = cmdsize - 8
+                fh.seek(cmdsize, 1)
+            else:
+                new_parser(endian, fh)
+            
+
 
     def parse_fatbin(self, endian, fh):
         self.w.tprint("FAT BINARY HEADER")
@@ -60,9 +128,9 @@ class Parser():
                 magic = unpack(">I", fh.read(4))[0]
                 (parser,endian) = self.parsers[magic]
                 parser(endian, fh)
-            except struct.error as se:
-                sys.stderr.write(f"Incorrect file format\n")
-                sys.exit(1)
+            #except struct.error as se:
+            #    sys.stderr.write(f"Incorrect file format\n")
+            #    sys.exit(1)
             except KeyError as ke:
                 sys.stderr.write(f"Incorrect magic bytes\n")
                 sys.exit(1)
--- macho_parser/parsers/constants.py
+++ macho_parser/parsers/constants.py
@@ -128,3 +128,174 @@ def get_cpu_type(type):
 
 def get_cpu_subtype(type, subtype):
     return cpu_types.get(type)["subtypes"][subtype]
+
+
+filetypes = {
+    0x1:    "MH_OBJECT",
+    0x2:    "MH_EXECUTE",
+    0x3:    "MH_FVMLIB",
+    0x4:    "MH_CORE",
+    0x5:    "MH_PRELOAD",
+    0x6:    "MH_DYLIB",
+    0x7:    "MH_DYLINKER",
+    0x8:    "MH_BUNDLE",
+    0x9:    "MH_DYLIB_STUB",
+    0xa:    "MH_DSYM",
+    0xb:    "MH_KEXT_BUNDLE"
+}
+
+def get_filetype(type):
+    return filetypes.get(type)
+
+flags = {
+    0x1:        "MH_NOUNDEFS",
+    0x2:        "MH_INCRLINK",
+    0x4:        "MH_DYLDLINK",
+    0x8:        "MH_BINDATLOAD",
+    0x10:       "MH_PREBOUND",
+    0x20:       "MH_SPLIT_SEGS",
+    0x40:       "MH_LAZY_INIT",
+    0x80:       "MH_TWOLEVEL",
+    0x100:      "MH_FORCE_FLAT",
+    0x200:      "MH_NOMULTIDEFS",
+    0x400:      "MH_NOFIXPREBINDING",
+    0x800:      "MH_PREBINDABLE",
+    0x1000:     "MH_ALLMODSBOUND",
+    0x2000:     "MH_SUBSECTIONS_VIA_SYMBOLS",
+    0x4000:     "MH_CANONICAL",
+    0x8000:     "MH_WEAK_DEFINES",
+    0x10000:    "MH_BINDS_TO_WEAK",
+    0x20000:    "MH_ALLOW_STACK_EXECUTION",
+    0x40000:    "MH_ROOT_SAFE",
+    0x80000:    "MH_SETUID_SAFE",
+    0x100000:   "MH_NO_REEXPORTED_DYLIBS",
+    0x200000:   "MH_PIE",
+    0x400000:   "MH_DEAD_STRIPPABLE_DYLIB"
+}
+
+def get_flags(flag):
+    res = ""
+    for k,v in flags.items():
+        if flag & k == k: 
+            if res != "":
+                res = res + " | "
+            res = res + v
+    return res
+
+command_types = {
+    0x00000001: "LC_SEGMENT",
+    0x00000002: "LC_SYMTAB",
+    0x00000003: "LC_SYMSEG",
+    0x00000004: "LC_THREAD",
+    0x00000005: "LC_UNIXTHREAD",
+    0x00000006: "LC_LOADFVMLIB",
+    0x00000007: "LC_IDFVMLIB",
+    0x00000008: "LC_IDENT",
+    0x00000009: "LC_FVMFILE",
+    0x0000000a: "LC_PREPAGE",
+    0x0000000b: "LC_DYSYMTAB",
+    0x0000000c: "LC_LOAD_DYLIB",
+    0x0000000d: "LC_ID_DYLIB",
+    0x0000000e: "LC_LOAD_DYLINKER",
+    0x0000000f: "LC_ID_DYLINKER",
+    0x00000010: "LC_PREBOUND_DYLIB",
+    0x00000011: "LC_ROUTINES",
+    0x00000012: "LC_SUB_FRAMEWORK",
+    0x00000013: "LC_SUB_UMBRELLA",
+    0x00000014: "LC_SUB_CLIENT",
+    0x00000015: "LC_SUB_LIBRARY",
+    0x00000016: "LC_TWOLEVEL_HINTS",
+    0x00000017: "LC_PREBIND_CKSUM",
+    0x00000019: "LC_SEGMENT_64",
+    0x0000001A: "LC_ROUTINES_64",
+    0x0000001B: "LC_UUID",
+    0x0000001D: "LC_CODE_SIGNATURE",
+    0x0000001E: "LC_SEGMENT_SPLIT_INFO",
+    0x00000020: "LC_LAZY_LOAD_DYLIB",
+    0x00000021: "LC_ENCRYPTION_INFO",
+    0x00000022: "LC_DYLD_INFO",
+    0x00000024: "LC_VERSION_MIN_MACOSX",
+    0x00000025: "LC_VERSION_MIN_IPHONEOS",
+    0x00000026: "LC_FUNCTION_STARTS",
+    0x00000027: "LC_DYLD_ENVIRONMENT",
+    0x00000029: "LC_DATA_IN_CODE",
+    0x0000002A: "LC_SOURCE_VERSION",
+    0x0000002B: "LC_DYLIB_CODE_SIGN_DRS",
+    0x0000002C: "LC_ENCRYPTION_INFO_64",
+    0x0000002D: "LC_LINKER_OPTION",
+    0x0000002E: "LC_LINKER_OPTIMIZATION_HINT",
+    0x0000002F: "LC_VERSION_MIN_TVOS",
+    0x00000030: "LC_VERSION_MIN_WATCHOS",
+    0x80000018: "LC_LOAD_WEAK_DYLIB",
+    0x8000001C: "LC_RPATH",
+    0x8000001F: "LC_REEXPORT_DYLIB",
+    0x80000022: "LC_DYLD_INFO_ONLY",
+    0x80000023: "LC_LOAD_UPWARD_DYLIB",
+    0x80000028: "LC_MAIN"
+}
+
+def get_command(cmd):
+    return command_types.get(cmd, "** NON STANDARD **")
+
+section_flags_base = {
+    0x00:       "S_REGULAR",
+    0x01:       "S_ZEROFILL",
+    0x02:       "S_CSTRING_LITERALS",
+    0x03:       "S_4BYTE_LITERALS",
+    0x04:       "S_8BYTE_LITERALS",
+    0x05:       "S_LITERAL_POINTERS",
+    0x06:       "S_NON_LAZY_SYMBOL_POINTERS",
+    0x07:       "S_LAZY_SYMBOL_POINTERS",
+    0x08:       "S_SYMBOL_STUBS",
+    0x09:       "S_MOD_INIT_FUNC_POINTERS",
+    0x0a:       "S_MOD_TERM_FUNC_POINTERS",
+    0x0b:       "S_COALESCED",
+    0x0c:       "S_GB_ZEROFILL",
+    0x0d:       "S_INTERPOSING",
+    0x0e:       "S_16BYTE_LITERALS",
+    0x0f:       "S_DTRACE_DOF",
+    0x10:       "S_LAZY_DYLIB_SYMBOL_POINTERS",
+    0x11:       "S_THREAD_LOCAL_REGULAR",
+    0x12:       "S_THREAD_LOCAL_ZEROFILL",
+    0x13:       "S_THREAD_LOCAL_VARIABLES",
+    0x14:       "S_THREAD_LOCAL_VARIABLE_POINTERS",
+    0x15:       "S_THREAD_LOCAL_INIT_FUNCTION_POINTERS"
+}
+
+section_flags_mask = {
+    0x00000100: "S_ATTR_LOC_RELOC",
+    0x00000200: "S_ATTR_EXT_RELOC",
+    0x00000400: "S_ATTR_SOME_INSTRUCTIONS",
+    0x02000000: "S_ATTR_DEBUG",
+    0x04000000: "S_ATTR_SELF_MODIFYING_CODE",
+    0x08000000: "S_ATTR_LIVE_SUPPORT",
+    0x10000000: "S_ATTR_NO_DEAD_STRIP",
+    0x20000000: "S_ATTR_STRIP_STATIC_SYMS",
+    0x40000000: "S_ATTR_NO_TOC",
+    0x80000000: "S_ATTR_PURE_INSTRUCTIONS"
+}
+
+def get_sflags(flag):
+    res = section_flags_base[flag & 0xf]
+    flag = flag & ~0xf
+    for k,v in section_flags_mask.items():
+        if flag & k == k: 
+            if res != "":
+                res = res + " | "
+            res = res + v
+    return res
+
+prots = {
+    0x01:   "VM_PROT_READ",
+    0x02:   "VM_PROT_WRITE",
+    0x04:   "VM_PROT_EXECUTE"
+}
+
+def get_prots(prot):
+    res = ""
+    for k,v in prots.items():
+        if prot & k == k: 
+            if res != "":
+                res = res + " | "
+            res = res + v
+    return res
